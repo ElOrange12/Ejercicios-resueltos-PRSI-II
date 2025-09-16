e12b48b13daafe90732787c32c9bceeee859a204
Dani <danieloliveiravidal12@gmail.com>
2025-09-16 10:48:45 +0200

    Create Ej70.py

---
+++ Ej70.py
@@ -0,0 +1,24 @@
+##############################################################################
+#                                                                            #
+#Ej70.py                                                                     #
+#                                                                            #
+#Autor:Daniel Oliveira Vidal                                                 #
+#                                                                            #
+#Fecha:5 de noviembre del 2024                                               #
+#                                                                            #
+#Proposito:Este programa identifica entre mayúsculas y minúsculas.           #
+#                                                                            #
+##############################################################################
+
+l = 0
+#"l" es la letra que preguntamos
+
+l = ord(input('Dime una letra del alfabeto: '))
+
+if l >= 65 and l <= 90:
+    print('Es una MAYÚSCULA')
+    I += 6
+
+elif l >= 97 and l <= 122:
+    print('Es una MINÚSCULA')
+    I += 6
