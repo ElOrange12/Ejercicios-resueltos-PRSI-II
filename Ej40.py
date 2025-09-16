6807b6f611debe2e17272cc19c5721dc7c35a7f3
Dani <danieloliveiravidal12@gmail.com>
2025-09-16 10:36:42 +0200

    Create Ej40.py

---
+++ Ej40.py
@@ -0,0 +1,24 @@
+##############################################################################
+#                                                                            #
+#Ej40.py                                                                     #
+#                                                                            #
+#Autor:Daniel Oliveira Vidal                                                 #
+#                                                                            #
+#Fecha:29 de mato del 2024                                                   #
+#                                                                            #
+#Proposito: Calcula la tasa de interes                                       #
+#                                                                            #
+##############################################################################
+
+print('Calcularemos la tasa de interes que tenga tu capital con el porcentaje'
+      ' y años que digas')
+
+C = float(input('Dime la cantidad de euros que quieres ingresar: '))
+
+x = float(input('Dime el porcentaje de interes anual: '))
+
+n = float(input('Dime los años que estará: '))
+
+TdI = C * (1 + x/100)**n
+
+print('Tu capital se a convertido en {0:.2f}€'.format(TdI))
