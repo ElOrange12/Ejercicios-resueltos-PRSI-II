934efc23e5c25357bbcbcdadcc31cb9f74f1e13f
Dani <danieloliveiravidal12@gmail.com>
2025-09-16 12:12:41 +0200

    Create CdM v3.0.2.py

---
+++ Calculadora de matrices/CdM v3.0.2.py
@@ -0,0 +1,156 @@
+##############################################################################
+#                                                                            #
+#CdM v3.0.2.py                                                               #
+#                                                                            #
+#Autor:Daniel Oliveira Vidal                                                 #
+#                                                                            #
+#Fecha:28 de marzo del 2024                                                  #
+#                                                                            #
+#Proposito:Este programa calcula operaciones con matrices.                   #
+#                                                                            #
+##############################################################################
+
+#Se presenta el programa
+print('Bienvenido a la calculadora de matrices')
+print(' ')
+
+R = 'S'
+#Este bucle es el encargado de seguir con el programa si el usuario quiere
+while R == 'S':
+
+    #Preguntamos que operacion quiere hacer:
+    O = 0
+    #Este bucle comprueba que se conteste una respuesta correcta.
+    while O < 1 or O > 5:
+        print('Que operación o operaciones quieres realizar:')
+        print('1 --> Suma de matrices.')
+        print('2 --> Resta de matrices.')
+        print('3 --> Multiplicación de matrices. (Work in progress)')
+        print('4 --> Determinante de una matriz. (Work in progress)')
+        print('5 --> Cerrar. ')
+        print('')
+
+        O = int(input('¿Qué quieres hacer?: '))
+        print('')
+
+        if O < 1 or O > 5:
+            print('Esa no es un acción posible')
+            print('')
+        elif O == 5:
+            R = 'N'
+    
+    if O == 1:
+        print('Para sumar matrices necesitaremos dos matrices y estan deben'
+              ' ser iguales, es decir, mismas filas y columnas.')
+        print('')
+        
+        FS = int(input('¿Cuantas filas tienen las matrices?: '))
+        CS = int(input('¿Cuantas columnas tienen las matrices?: '))
+        print('')
+
+        #Creamos la primera matriz
+        print('----> Introduce la primera matriz')
+        M1 = []
+
+        #Ahora preguntamos la matriz
+        for i in range(FS):
+            M1.append([0] * CS)
+
+        for j in range(FS):
+            for k in range(CS):
+                M1[j][k] = float(input('Dime el número que esta en F{0:.0f} '
+                                       'C{1:.0f}: '.format(j+1, k+1)))
+        print('')
+
+        #Creamos la segunda matriz
+        print('----> Introduce la segunda matriz')
+        M2 = []
+
+        #Ahora preguntamos la matriz
+        for i in range(FS):
+            M2.append([0] * CS)
+
+        for j in range(FS):
+            for k in range(CS):
+                M2[j][k] = float(input('Dime el número que esta en F{0:.0f} '
+                                       'C{1:.0f}: '.format(j+1, k+1)))
+        print('')
+
+        #Creamos la matriz resultante
+        MT = []
+
+        #Ahora calculamos la matriz resultante
+        for i in range(FS):
+            MT.append([0] * CS)
+
+        for j in range(FS):
+            for k in range(CS):
+                MT[j][k] = M1[j][k] + M2[j][k]
+        
+        #Ahora la mostramos en pantalla
+        for i in range(FS):
+            print(MT[i])
+
+    elif O == 2:
+        print('Para restar matrices necesitaremos dos matrices y estan deben'
+              ' ser iguales, es decir, mismas filas y columnas.')
+        print('')
+        
+        FR = int(input('¿Cuantas filas tienen las matrices?: '))
+        CR = int(input('¿Cuantas columnas tienen las matrices?: '))
+        print('')
+
+        #Creamos la primera matriz
+        print('----> Introduce la primera matriz')
+        M1 = []
+
+        #Ahora preguntamos la matriz
+        for i in range(FR):
+            M1.append([0] * CR)
+
+        for j in range(FR):
+            for k in range(CR):
+                M1[j][k] = float(input('Dime el número que esta en F{0:.0f} '
+                                       'C{1:.0f}: '.format(j+1, k+1)))
+        print('')
+
+        #Creamos la segunda matriz
+        print('----> Introduce la segunda matriz')
+        M2 = []
+
+        #Ahora preguntamos la matriz
+        for i in range(FR):
+            M2.append([0] * CR)
+
+        for j in range(FR):
+            for k in range(CR):
+                M2[j][k] = float(input('Dime el número que esta en F{0:.0f} '
+                                       'C{1:.0f}: '.format(j+1, k+1)))
+        print('')
+
+        #Creamos la matriz resultante
+        MT = []
+
+        #Ahora calculamos la matriz resultante
+        for i in range(FR):
+            MT.append([0] * CR)
+
+        for j in range(FR):
+            for k in range(CR):
+                MT[j][k] = M1[j][k] - M2[j][k]
+        
+        #Ahora la mostramos en pantalla
+        for i in range(FR):
+            print(MT[i])
+
+    elif O == 3:
+        print('Esta función esta aun en procesa no es posible ejecutarla.')
+        print('')
+
+    elif O == 4:
+        print('Esta función esta aun en procesa no es posible ejecutarla.')
+        print('')
+
+    if R == 'S':           
+        print('¿Quieres seguir operando?')
+        R = input('para seguir escribe "S" y para terminar "N": ')
