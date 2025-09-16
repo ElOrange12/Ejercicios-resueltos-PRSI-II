74b2dc3e703ff851e0266bc2de877b8c8f3586a4
Dani <danieloliveiravidal12@gmail.com>
2025-09-16 10:44:31 +0200

    Create Ej57.py

---
+++ Ej57.py
@@ -0,0 +1,18 @@
+##############################################################################
+#                                                                            #
+#Ej57.py                                                                     #
+#                                                                            #
+#Autor:Daniel Oliveira Vidal                                                 #
+#                                                                            #
+#Fecha:15 de mayo del 2024                                                   #
+#                                                                            #
+#Proposito:Te dice si un numero es negativo                                  #
+#                                                                            #
+##############################################################################
+print('Te preguntaremos un número y te diremos si es negativo')
+
+N = float(input('Dime el numero: '))
+#"N" es el numero que nos dicen
+
+if N < 0:
+    print('El número es negativo')
